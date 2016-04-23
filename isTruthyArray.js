e37d5785fc5bca0a42b445e941ec8163efc94cd6
Will Wright pc <wawwright@gmail.com>
2016-04-22 22:28:58 -0600

    reformatted

--- isTruthyArray.js
+++ isTruthyArray.js
@@ -1,15 +1,15 @@
 // takes an Array and opperates on it with the  given function returns new  Array
-function forArray(myArray){
+function forArray(myArray) {
 	var out = [];
-	for (var i = 0 ; i < myArray.length; i++){
-		if(!!myArray[i]){
-		 out.push(myArray[i]);
+	for (var i = 0; i < myArray.length; i++) {
+		if (!!myArray[i]) {
+			out.push(myArray[i]);
 		}
 	}
 	return out;
 }
 
-var a = [1,2,3,"",0,true,0,1,2,3,false,0,NaN,undefined,null,"foo"];
+var a = [1, 2, 3, "", 0, true, 0, 1, 2, 3, false, 0, NaN, undefined, null, "foo"];
 
 var b = forArray(a);
 console.log(b);
